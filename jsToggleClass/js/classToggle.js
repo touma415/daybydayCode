21d26bfbd0aed02578a23b836c5f631c519df9b6
touma415 <touma415@gmail.com>
2019-07-25 16:47:38 +0900

    0725 cssプロバティ値を取得して加算し結果を返すjs 歩行アニメーションがやりたかった

--- jsToggleClass/js/classToggle.js
+++ jsToggleClass/js/classToggle.js
@@ -2,3 +2,26 @@ function toggleClass() {
   var e = document.getElementsByClassName("class-default");
   e[0].classList.toggle("class-toggled");
 }
+
+function toggleClass2() {
+  var e = document.getElementsByClassName("class-default");
+  var classdefault = document.querySelector(".class-default");
+  var classtoggled = document.querySelector(".class-toggled");
+
+  console.log(e);
+
+  console.log(classdefault);
+  console.log(classtoggled);
+
+  var left = parseInt(classdefault.style.left);
+
+  console.log(left);
+
+  classdefault.style.left = left + 20 + "px";
+
+  console.log(classdefault.style.left);
+
+  classtoggled.style.left = left + 20 + "px";
+
+  e[0].classList.toggle("class-toggled");
+}
