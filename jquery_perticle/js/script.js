2bfa8cfcaccbed1e38f32693cc27ab57f5d560ca
touma415 <touma415@gmail.com>
2019-07-30 10:23:49 +0900

    0730　particle.jsを使ったパーティクルが舞う背景

---
+++ jquery_perticle/js/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  Particles.init({
+    selector: ".bg",
+    sizeVariations: 30,
+    color: ["#7fffff", "rgba(0,187,211,0.5)", "rgba(255,127,255,0.3)"]
+    // connectParticles: true
+  });
+};
