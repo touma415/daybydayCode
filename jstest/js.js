f1468769342bfaad2c45831be79ab0fd2b5afddd
touma415 <touma415@gmail.com>
2019-08-01 13:16:46 +0900

    更新されてなかったので直し

--- jstest/js.js
+++ jstest/js.js
@@ -1,9 +1,48 @@
-let button1 = document.getElementsById("button1");
+// button1
+let button1 = document.getElementById("button1");
 
 button1.addEventListener(
-  "click",
+  "click", //クリックイベントalert処理
   function() {
-    alart("クリックイベントの処理");
+    alert("クリックイベントの処理");
+  },
+  false
+);
+
+let button2 = document.getElementById("button2");
+
+button2.addEventListener(
+  "mousedown", //押している間に発動
+  function() {
+    button2.style =
+      "background-image: linear-gradient(to top, #fad0c4 0%, #ffd1ff 100%);";
+  },
+  false
+);
+
+button2.addEventListener(
+  "mouseup", //押しているのを離したときに発動
+  function() {
+    button2.style =
+      " background-image: linear-gradient(120deg, #a6c0fe 0%, #f68084 100%);";
+  },
+  false
+);
+
+let button3 = document.getElementById("button3");
+
+button3.addEventListener(
+  "keypress",
+  function() {
+    button3.style = "transform: rotate(360deg);transition: 0.5s;";
+  },
+  false
+);
+
+button3.addEventListener(
+  "keyup",
+  function() {
+    button3.style = "transform: rotate(0deg);transition: 0.5s;";
   },
   false
 );
