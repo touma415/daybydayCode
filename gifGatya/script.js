114e3758398ed5ea5f2b19ab7a43d1a2fc9148ce
touma415 <touma415@gmail.com>
2019-08-14 17:56:40 +0900

    gifガチャ

---
+++ gifGatya/script.js
@@ -0,0 +1,10 @@
+const playButton = document.getElementById("play");
+const resultBlock = document.getElementById("result");
+const gifImg = document.getElementById("gif");
+
+playButton.addEventListener("click", function() {
+  /* Insert code here... */
+  axios.get("https://ruddy-mail.glitch.me/api/gacha").then(response => {
+    /* Insert code here... */
+  });
+});
